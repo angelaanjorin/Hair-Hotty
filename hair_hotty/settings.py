ee8d5cfc725687b39bcc8151dc6e5f783ac65fbb
Angela Anjorin <angela.anjorin@gmail.com>
2024-11-15 18:46:15 +0000

    Setup webhook handler.

--- hair_hotty/settings.py
+++ hair_hotty/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-s9&07hz&4v54xejc0esml70tr%1w-2f0341j7ff*&$qlq=v$@_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEVELOPMENT = os.environ.get('DEVELOPMENT', False)
