dc307a5c6afaf68c8e8d178f121d994286692a92
Angela Anjorin <angela.anjorin@gmail.com>
2024-11-15 11:30:57 +0000

    Add loading overlay functionality to checkout template.

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,7 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
+
     if request.method == 'POST':
         bag = request.session.get('bag', {})
 
@@ -72,7 +73,9 @@ def checkout(request):
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
         stripe_total = round(total * 100)
-        stripe.api_key = stripe_secret_key
+        
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+
         intent = stripe.PaymentIntent.create(
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
