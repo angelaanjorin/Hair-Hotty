5d04156a8aa594b42a0e98df4e20144e7539ebf1
Angela Anjorin <angela.anjorin@gmail.com>
2024-11-18 21:36:06 +0000

    Product Admin: Add the delete functionality

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<uuid:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<uuid:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<uuid:product_id>/', views.delete_product, name='delete_product'),
 ]
