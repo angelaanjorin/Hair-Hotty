a489acf55a28f654a68b93e63f0d2e025cb7e9a9
Angela Anjorin <angela.anjorin@gmail.com>
2024-11-26 15:51:20 +0000

    Remove default_file_storage

--- hair_hotty/settings.py
+++ hair_hotty/settings.py
@@ -222,9 +222,9 @@ if 'USE_AWS' in os.environ:
         "BACKEND": "custom_storages.MediaStorage",
     },
     "staticfiles": {"BACKEND": "custom_storages.StaticStorage"},}
-    
+
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    #DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
