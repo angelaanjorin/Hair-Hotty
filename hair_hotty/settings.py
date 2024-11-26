702b2920e67009c2299a40450e1f3c51afac9d54
Angela Anjorin <angela.anjorin@gmail.com>
2024-11-26 14:19:51 +0000

    Bug: Deployment not working to Heroku- Fix1: Add whitenoise, now only the static files are giving a 404 error but can be run locally.

--- hair_hotty/settings.py
+++ hair_hotty/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -84,7 +85,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'hair_hotty.urls'
