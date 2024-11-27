dcb321c4ea59e4f8a053d2cef24f57c52ae083f4
Angela Anjorin <angela.anjorin@gmail.com>
2024-11-27 18:58:08 +0000

    Bufix: Try to fix email not sending on heroku

--- hair_hotty/settings.py
+++ hair_hotty/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEVELOPMENT = os.environ.get('DEVELOPMENT', False)
-DEBUG = DEVELOPMENT
-DEBUG = False
+#DEVELOPMENT = os.environ.get('DEVELOPMENT', False)
+#DEBUG = DEVELOPMENT
+DEBUG = True
 
 
 CSRF_TRUSTED_ORIGINS = [
