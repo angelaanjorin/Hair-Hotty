233f1c7ebfc0f86637c2923e6180ea406e72f184
Angela Anjorin <angela.anjorin@gmail.com>
2024-11-27 19:29:18 +0000

    Change url links for the error templates

--- hair_hotty/views.py
+++ hair_hotty/views.py
@@ -10,14 +10,14 @@ def handler404(request, exception):
     '''
     Render 404 page
     '''
-    return render(request, '404.html', status=404)
+    return render(request, 'errors/404.html', status=404)
 
 
 def handler500(request):
     '''
     Render 500 page
     '''
-    return render(request, '500.html', status=500)
+    return render(request, 'errors/500.html', status=500)
 
 
 def handler403(request, exception):
@@ -25,6 +25,6 @@ def handler403(request, exception):
     Render 403 page
     '''
     if isinstance(exception, PermissionDenied):
-        return render(request, '403.html', status=403)
+        return render(request, 'errors/403.html', status=403)
     else:
-        return render(request, '500.html', status=500)
+        return render(request, 'errors/500.html', status=500)
