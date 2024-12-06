06c840457f2ced5c5195e2118dd8d185f6a324e5
Angela Anjorin <angela.anjorin@gmail.com>
2024-12-06 09:30:59 +0000

    Add hair products to shop

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Product, PrimaryCategory, SpecialCategory
 
 
 # Register your models here.
 
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(SummernoteModelAdmin):
     list_display = (
         'name',
         'primary_category',
@@ -13,6 +14,7 @@ class ProductAdmin(admin.ModelAdmin):
         'rating',
         'image',
     )
+    summernote_fields = ('description',)
 
     ordering = ('name',)
 
