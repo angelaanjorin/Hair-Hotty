a35eb9a69168b593fb4f15e3414f4487fe96f2b3
Angela Anjorin <angela.anjorin@gmail.com>
2024-12-06 11:16:29 +0000

    Added Media url and root

--- hair_hotty/settings.py
+++ hair_hotty/settings.py
@@ -216,6 +216,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 #if DEVELOPMENT:
 #    # Local storage settings for development
 #    STATIC_URL = '/static/'
