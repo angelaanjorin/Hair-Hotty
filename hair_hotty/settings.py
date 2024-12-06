173eb238d90bbf5596ae55bd47635de25c349f0f
Angela Anjorin <angela.anjorin@gmail.com>
2024-12-06 12:31:24 +0000

    Recreated sitemap after blog app addition.

--- hair_hotty/settings.py
+++ hair_hotty/settings.py
@@ -219,17 +219,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-#if DEVELOPMENT:
-#    # Local storage settings for development
-#    STATIC_URL = '/static/'
-#    STATICFILES_DIRS =  (os.path.join(BASE_DIR, 'static'),)
-#    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
-#    MEDIA_URL = '/media/'
-#    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-#else:
-
 if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
@@ -244,14 +233,6 @@ if 'USE_AWS' in os.environ:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    # Static and media files
-#    STORAGES = {
-#        "default": {"BACKEND": "custom_storages.MediaStorage"},
-#        "staticfiles": {"BACKEND": "custom_storages.StaticStorage"},
-#    }
-
-#    STATICFILES_LOCATION = 'static'
-#    MEDIAFILES_LOCATION = 'media'
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
@@ -261,8 +242,6 @@ if 'USE_AWS' in os.environ:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
-#if not DEVELOPMENT:
-#    print(f"Using S3 with bucket: {AWS_STORAGE_BUCKET_NAME}")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
