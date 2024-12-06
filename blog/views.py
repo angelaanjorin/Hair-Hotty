17e2ba6fc43c0662d5cb4cb61cf08caf6b6bcb7b
Angela Anjorin <angela.anjorin@gmail.com>
2024-12-06 20:03:23 +0000

    Bugfix: Had to retrieve deleted blog.views from github because i mistakenly added the view bag.view into the blog.view.

--- blog/views.py
+++ blog/views.py
@@ -1,137 +1,213 @@
-from django.shortcuts import (
-    render, redirect, reverse, HttpResponse, get_object_or_404)
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib.auth.decorators import login_required
+from django.views import generic, View
+from django.views.generic import ListView, CreateView, UpdateView
 from django.contrib import messages
-
-from products.models import Product
-
-
-# Create your views here.
-def view_bag(request):
-    """ A view that renders the bag contents page """
-
-    # Tells toast message which page user is on
-    # To set toast bag summary button link
-    context = {
-        'on_bag_page': True
-    }
-
-    return render(request, 'bag/bag.html', context)
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
+from .forms import CommentForm, PostForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from profiles.models import UserProfile
 
 
-def add_to_bag(request, item_id):
-    """Add a quantity of the specified product to the shopping bag."""
-    product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    size = None
+# Create or edit a post.
+class PostCreateOrUpdateView(
+        LoginRequiredMixin, UserPassesTestMixin, CreateView, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/add_post.html'
+    success_url = reverse_lazy('home')
 
-    # Choose the correct price based on sale status
-    price = (
-        product.sale_price if product.on_sale and product.sale_price
-        else product.price
-    )
+    def form_valid(self, form):
+        # Check if this is an update or a new creation
+        if self.get_object():
+            form.instance.author = self.request.user
+            response = super().form_valid(form)
+            messages.success(self.request, "Post updated successfully!")
 
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
-    bag = request.session.get('bag', {})
+            return redirect(reverse_lazy('profile',
+                            kwargs={'pk': form.instance.author.profile.pk}))
 
-    if size:
-        if item_id in list(bag.keys()):
-            if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
-                messages.success(
-                    request, f'Updated size {size.upper()} {product.name} \
-                    quantity to {bag[item_id]["items_by_size"][size]}')
-            else:
-                bag[item_id]['items_by_size'][size] = quantity
-                messages.success(
-                    request, f'Added size {size.upper()} {product.name} \
-                    to your bag')
-        else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(
-                request, f'Added size {size.upper()} {product.name} \
-                to your bag')
+        else:  # If no object exists, it´s a new job creation
+            form.instance.author = self.request.user
+            response = super().form_valid(form)
+
+            if self.object.status == 0:
+                messages.info(
+                    self.request,
+                    'Your Post has been submitted and is awaiting approval.')
+
+        return response
+
+    def test_func(self):
+        return self.request.user.profile
+
+    def get_object(self, get_queryset=None):
+        post_id = self.kwargs.get('pk')
+        if post_id:
+            return Post.objects.get(pk=post_id)
+        return None
+
+
+# Post delete view for all registered users
+def post_delete_view(request, pk):
+    """ View to delete a post
+    """
+    # Fetch the post using the primary key(pk)
+    post = get_object_or_404(Post, pk=pk, author=request.user)
+
+    # check if the current user is the author of the Post
+    if post.author == request.user:
+        post.delete()
+        messages.success(request, "Post deleted successfully!")
     else:
-        # Add to bag logic
-        if item_id in list(bag.keys()):
-            bag[item_id] += quantity
-            messages.success(
-                request, f'Updated {product.name} quantity to {bag[item_id]}')
+        messages.error(request, "You can only delete your own post")
+
+    return redirect('profile', pk=request.user.pk)
+
+
+class PostList(generic.ListView):
+    """
+    Returns all published posts in :model:`blog.Post`
+    and displays them in a page of six posts.
+    **Context**"""
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(PostList, self).get_context_data(*args, **kwargs)
+
+        return context
+
+
+class post_detail(View):
+    """
+    Display an individual :model:`blog.Post`.
+
+    """
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.all().order_by("-created_on")
+        comment_count = post.comments.all().count()
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "blog/post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked,
+                "comment_form": CommentForm(),
+            },)
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Your comment has been posted and you can edit or delete it!'
+            )
+            # Resets the form after comment submission
+            comment_form = CommentForm()
         else:
-            bag[item_id] = quantity
-            messages.success(request, f"Added {product.name} to your bag.")
+            messages.add_message(
+                request, messages.ERROR, "Error posting your comment."
+            )
 
-    # Update the session
-    request.session['bag'] = bag
-    return redirect(redirect_url)
+        # Re-fetch the comments and comment count after the
+        # new comment is posted
+        comments = post.comments.all().order_by("-created_on")
+        comment_count = comments.count()
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "blog/post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "comment_count": comment_count,
+                "comment_form": comment_form,
+                "liked": liked
+            },
+        )
 
 
-def adjust_bag(request, item_id):
-    """Adjust the quantity of the specified product to the specified amount"""
+def comment_edit(request, slug, comment_id):
+    """View to edit comments"""
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
 
-    product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
-
-    # Choose the correct price based on sale status
-    price = (
-        product.sale_price if product.on_sale and product.sale_price
-        else product.price
-    )
-
-    bag = request.session.get('bag', {})
-
-    if size:
-        if quantity > 0:
-            bag[item_id]['items_by_size'][size] = quantity
-            messages.success(
-                request, f'Updated size {size.upper()} {product.name} \
-                quantity to {bag[item_id]["items_by_size"][size]}')
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST, instance=comment)
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            messages.add_message(request,
+                                 messages.SUCCESS,
+                                 'Comment updated successfully!')
         else:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} \
-            {product.name} from your bag')
+            messages.add_message(request,
+                                 messages.ERROR,
+                                 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+def comment_delete(request, slug, comment_id):
+    """
+    view to delete comment
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request,
+                             messages.SUCCESS,
+                             'Your comment has been deleted!')
     else:
-        if quantity > 0:
-            bag[item_id] = quantity
-            messages.success(request, f'Updated {product.name} quantity to \
-            {bag[item_id]}')
+        messages.add_message(request,
+                             messages.ERROR,
+                             'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+class PostLike(View):
+    """
+    Toggles like status on submission of like button on posts.
+    Also sends success message to author
+    Login required
+    """
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
         else:
-            bag.pop(item_id)
-            messages.success(request, f'Removed {product.name} from your bag')
+            post.likes.add(request.user)
+            messages.add_message(request,
+                                 messages.SUCCESS,
+                                 'Thanks for liking the post!')
 
-    request.session['bag'] = bag
-    return redirect(reverse('view_bag'))
-
-
-def remove_from_bag(request, item_id):
-    """Remove the item from the shopping bag"""
-
-    try:
-        product = get_object_or_404(Product, pk=item_id)
-        size = None
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
-        bag = request.session.get('bag', {})
-
-        if size:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} \
-            {product.name} from your bag')
-        else:
-            bag.pop(item_id)
-            messages.success(request, f'Removed {product.name} from your bag')
-
-        request.session['bag'] = bag
-        return HttpResponse(status=200)
-
-    except Exception as e:
-        messages.error(request, f'Error removing item: {e}')
-        return HttpResponse(status=500)
+        return HttpResponseRedirect(reverse(
+            'post_detail', args=[slug]))
